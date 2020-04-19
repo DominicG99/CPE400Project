3a1414075b19a94574c376122ba84f403688b195
DominicG99 <dominicginter@nevada.unr.edu>
2020-04-19 16:44:45 -0700

    Did a lot of stuff.....

--- CPEProject/Main.py
+++ CPEProject/Main.py
@@ -1,9 +1,13 @@
 # Authors: Dominic Ginter, <Insert Name 2>, <Insert Name 3>
 # CPE 400 Project
 # Hierarchial Routing Simulation
+import matplotlib as plt
 import math
 import networkx as nx # This is for creating graphs
-network=nx.Graph()
+network = nx.MultiGraph()
+network.add_nodes_from([0,36])
+network.add_edges_from([(0,1),(0,3),(1,2),(2,3)], weight = 1.0) #Cluster 1 Region 1 Connections
+network.add_edges_from([(4,5),(4,7), (5,6), (6,7)], weight = 1.0) #Cluster 1 Region 2 Connections
 print("This program simulates Multilevel hierarchial routing with an example network.\nThis example network can be found inside the project folder.\n") #Brief Description
 while True: # This will keep the program running until the user decides to quit.
     print("Please select an option.\n1.Route Packets in Network\n2.Display Routing Table\n3.List Border Gateway Routers\n4.Exit Program") #Menu print
@@ -27,7 +31,7 @@ while True: # This will keep the program running until the user decides to quit.
             # Will print the hierarchial routing table here
         break
     elif cmd == '3':
+        print("I'm thinking we make a function to list the level 1 and 2 BG routers")
         break
     elif cmd == '4':
-        print("I'm thinking we make a function to list the level 1 and 2 BG routers")
         break
