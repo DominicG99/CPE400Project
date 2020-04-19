440cfafa49292c424a2dc187c9562f6836d93533
ls <dominicginter@nevada.unr.edu>
2020-04-19 12:48:42 -0700

    Some changes

--- CPEProject/Main.py
+++ CPEProject/Main.py
@@ -1,4 +1,15 @@
 import math
 
-
-print("Please select an option.\n1.Route packets in network\n2.Display routing table\n3.Exit Program")
+flag = True
+while flag:
+    print("1.Route packets in network\n2.Display routing table\n3.Exit Program\nPlease select an option.")
+    cmd = input()
+    if cmd == '1':
+        print("Enter a source address: ")
+        srcAdd = input()
+        print("Enter a destination address: ")
+        dstAdd = input()
+    elif cmd == '2':
+        print("Do something else here")
+    elif cmd == '3':
+        flag = False
