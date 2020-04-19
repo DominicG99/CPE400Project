0a38074e66dafd7734469a76d7eb4b2cefe39175
ls <dominicginter@nevada.unr.edu>
2020-04-19 13:21:38 -0700

    More changes

--- CPEProject/Main.py
+++ CPEProject/Main.py
@@ -1,15 +1,17 @@
 import math
-
-flag = True
-while flag:
-    print("1.Route packets in network\n2.Display routing table\n3.Exit Program\nPlease select an option.")
+print("This program simulates Multilevel hierarchial routing with an example network.\nThis example network can be found inside the project folder.\n")
+while True:
+    print("Please select an option.\n1.Route packets in network\n2.Display routing table\n3.Exit Program")
     cmd = input()
     if cmd == '1':
         print("Enter a source address: ")
         srcAdd = input()
         print("Enter a destination address: ")
         dstAdd = input()
+        break
     elif cmd == '2':
-        print("Do something else here")
+        print("Enter a router address: ")
+        rtrAdd = input()
+        break
     elif cmd == '3':
-        flag = False
+        break
