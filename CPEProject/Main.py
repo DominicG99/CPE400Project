b808ede3934a168b6aedc85a27f876df50d2da59
DominicG99 <dominicginter@nevada.unr.edu>
2020-04-21 14:46:24 -0700

    fixed some table formatting

--- CPEProject/Main.py
+++ CPEProject/Main.py
@@ -1,7 +1,6 @@
-# Authors: Dominic Ginter, <Insert Name 2>, <Insert Name 3>
+# Authors: Dominic Ginter, Cole Atkinson, <Insert Name 3>
 # CPE 400 Project
 # Hierarchial Routing Simulation
-#from prettytable import PrettyTable
 import matplotlib.pyplot as plt
 import math
 import Graph
@@ -12,16 +11,16 @@ from prettytable import PrettyTable #Will use this for creating tables in Option
 nx.draw_networkx(Graph.HNetwork,with_labels=True,node_size=80, font_size = 8, fontweight = 'bold', figsize = (60, 60)) #This just makes an image of the graph we have with networkx.
 print("This program simulates Multilevel hierarchial routing with an example network.\nThis example network can be found inside the project folder.\n") #Brief Description
 while True: # This will keep the program running until the user decides to quit.
-    print("Please select an option.\n1.Route Packets in Network\n2.Display Routing Table\n3.List Border Gateway Routers\n4.Display Mathematical Nodal map \n5.Display realistic nodal map \n6.Exit program \n") #Menu print
+    print("Please select an option.\n1.Route Packets in Network\n2.Display Routing Table\n3.List Border Gateway Routers\n4.Display Mathematical Nodal map \n5.Display Realistic Nodal map \n6.Exit program \n") #Menu print
     cmd = input() #collect user input
-    if cmd == '1':
+    if cmd == '1': #Option 1
         print("Enter a source address: ")
         srcAdd = input() # User will enter source router address from example network
         print("Enter a destination address: ")
         dstAdd = input() # User will enter destination router address from example network
         print(nx.dijkstra_path(Graph.HNetwork, srcAdd, dstAdd))
         print ("This path takes a total of ", nx.shortest_path_length(Graph.HNetwork, srcAdd, dstAdd, weight= 'weight'), " hop(s).")
-    elif cmd == '2':
+    elif cmd == '2': #Option 2
         print("Enter a router address: ") # User will enter a router address from example network
         rtrAdd = input()
         print("Would you like to see the full table or hierarchial table? (Please type 1 for full table or 2 for hierarchial table.") #This will show the difference between full table and hierarchial table
@@ -32,14 +31,14 @@ while True: # This will keep the program running until the user decides to quit.
         elif tableChoice == '2':
             print("In the works.")
             # Will print the hierarchial routing table here
-    elif cmd == '3':
+    elif cmd == '3': #Option 3
         print("I'm thinking we make a function to list the level 1 and 2 BG routers")
-    elif cmd == '4':
+    elif cmd == '4': #Option 4
         print("Displaying the mathematical nodal map...")
-        plt.show()
-    elif cmd == '5':
+        plt.show() #Opens the Nodal Map
+    elif cmd == '5': #Option 5
         print("Displaying the realistic nodal map")
-        realisticImage = Image.open('Example network.jpg')
-        realisticImage.show()
-    elif cmd == '6':
+        realisticImage = Image.open('Example network.jpg') #Opens the example network image
+        realisticImage.show() #Displays the example network image
+    elif cmd == '6': #Exit
         break
--- CPEProject/Table.py
+++ CPEProject/Table.py
@@ -6,15 +6,5 @@ import networkx as nx
 from prettytable import PrettyTable
 x = PrettyTable()
 nextAdd = Graph.HNetwork.neighbors(rtrAdd) #This is necessary for the next hop address, but it displays a whack ass error
-x.field_names = ["Network ID", "Subnet Mask", "Next Hop", "Outgoing Interface", "Metric"]
-#Network ID is the IP address being analyzed in the row of the table
-#Subnet Mask is what I found online about class a ip addresses, which is what i am assuming these addresses are under since they all start with 1-3
-#Next hop is the next chronological hop, not necessarily the cheapest hop
-#Outgoing Interface is confusing, I am assuming it means where the IP address is located in the Example network.jpg
-#Metric represents the amount of hops it takes to get to the NEXT address
-
-#The table works as it needs to, however, Next hop is almost complete I just need to figure out how to debug the whack ass error im getting. I need to fix outgoing interface and metric
-
-x.add_row([rtrAdd, "255.0.0.0", rtrAdd, "Cluster 1 - Region 1", "1"])
-
+x.field_names = ["Destination", "Line", "Weight"]
 print(x)
