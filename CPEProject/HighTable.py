b7b3adfd3e7e12272d0cf5b297f784f821e4da10
Cole <coletknsn@gmail.com>
2020-04-24 07:51:01 -0700

    updated comments on both table files

--- CPEProject/HighTable.py
+++ CPEProject/HighTable.py
@@ -8,15 +8,15 @@ from prettytable import PrettyTable
 
 x = PrettyTable()
 i = 0
-nextAdd = list(Graph.HNetwork.neighbors(rtrAdd))
-nextAddCount = len(nextAdd)
+nextAdd = list(Graph.HNetwork.neighbors(rtrAdd)) # nextAdd represents the next address, or the next hop of the inputted address
+nextAddCount = len(nextAdd) # nextAddCount represents the amount of hops it takes to get to the neighbor
 x.field_names = ["Destination", "Line", "Weight"]
 for y in range(nextAddCount):
     j = 0
-    weight = nx.dijkstra_path_length(Graph.HNetwork, rtrAdd, nextAdd[i])
-    rude = nx.dijkstra_path(Graph.HNetwork, rtrAdd, nextAdd[i])
+    weight = nx.dijkstra_path_length(Graph.HNetwork, rtrAdd, nextAdd[i]) # weight is the amount of hops it takes to get from the inputted address to the next address in the array of addresses
+    line = nx.dijkstra_path(Graph.HNetwork, rtrAdd, nextAdd[i]) # line represents the next hop to take after the inputted address
     try:
-        x.add_row([nextAdd[i], rude[1], weight])
+        x.add_row([nextAdd[i], line[1], weight])
     except IndexError:
         pass
     i = i + 1
--- CPEProject/Table.py
+++ CPEProject/Table.py
@@ -8,14 +8,14 @@ from prettytable import PrettyTable
 x = PrettyTable()
 i = 0
 j = 0
-allAdd = list(Graph.HNetwork)
+allAdd = list(Graph.HNetwork) #allAdd represents all addresses; it contains every node address in the graph
 x.field_names = ["Destination", "Line", "Weight"]
 for y in range(len(allAdd)) :
     j = 0
-    weight = nx.dijkstra_path_length(Graph.HNetwork, rtrAdd, allAdd[i])
-    rude = nx.dijkstra_path(Graph.HNetwork, rtrAdd, allAdd[i])
+    weight = nx.dijkstra_path_length(Graph.HNetwork, rtrAdd, allAdd[i]) # weight is the amount of hops it takes to get from the inputted address to the next address in the array of addresses
+    line = nx.dijkstra_path(Graph.HNetwork, rtrAdd, allAdd[i]) # line represents the next hop to take after the inputted address
     try:
-        x.add_row([allAdd[i], rude[1], weight])
+        x.add_row([allAdd[i], line[1], weight])
     except IndexError:
         pass
     i = i + 1
